d3f63ff2f7d97ffa4eb2fac74db9a5be0224c0d7
Eugene Horohorin <genixoid@gmail.com>
2020-04-01 23:18:00 +0400

    contact

--- bot.py
+++ bot.py
@@ -17,6 +17,27 @@ def get_param(section, option):
 	return result
 
 
+
+REASONS = []
+REASONS.append('Посещение суда')
+REASONS.append('Доставка несовершеннолетних в (из) образовательные организации')
+REASONS.append('Посещение медицинской или ветеринарной организации')
+REASONS.append('Участие в похоронах')
+REASONS.append('Восстановление паспорта')
+REASONS.append('Выезд в загородный дом или из него')
+REASONS.append('Посещение кредитных организаций и почтовых отделений')
+REASONS.append('Доставка лекарств, продуктов питания и предметов первой необходимости')
+REASONS.append('Изменение места проживания')
+
+
+reason_markup = types.ReplyKeyboardMarkup(row_width=2)
+for reason in REASONS:
+	itembtn = types.KeyboardButton(reason)
+	reason_markup.add(itembtn)
+	
+contact_request_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+button = types.KeyboardButton(text='Предоставить номер телефона',request_contact=True)
+contact_request_markup.add(button)
 	
 
 conf=configparser.ConfigParser()
@@ -26,6 +47,7 @@ conf.read("bot.cfg")
 DEBUG="true" == get_param("main", "DEBUG")
 
 
+
 TOKEN=get_param("telegram","TOKEN")
 if not TOKEN: 
 	print("Please, define the TOKEN param of bot.cfg file!")
@@ -33,6 +55,7 @@ if not TOKEN:
 
 if DEBUG:
 	print("DEBUG is on!")
+	print(REASONS)
 	print("TOKEN %s is used for connecting" % TOKEN)
 
 
@@ -47,18 +70,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	markup = types.ReplyKeyboardMarkup(row_width=2)
-	itembtn1 = types.KeyboardButton('Посещение суда')
-	itembtn2 = types.KeyboardButton('Доставка несовершеннолетних в (из) образовательные организации')
-	itembtn3 = types.KeyboardButton('Посещение медицинской или ветеринарной организации')
-	itembtn4 = types.KeyboardButton('Участие в похоронах')
-	itembtn5 = types.KeyboardButton('Восстановление паспорта')
-	itembtn6 = types.KeyboardButton('Выезд в загородный дом или из него')
-	itembtn7 = types.KeyboardButton('Посещение кредитных организаций и почтовых отделений')
-	itembtn8 = types.KeyboardButton('Доставка лекарств, продуктов питания и предметов первой необходимости')
-	itembtn9 = types.KeyboardButton('Изменение места проживания')
-	markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, itembtn9)
-	bot.send_message(message.chat.id, 'Привет, для оформления электронного пропуска укажите пожалуйста причину:', reply_markup=markup)
+	bot.send_message(message.chat.id, 'Please Chose One :', reply_markup=contact_request_markup)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -66,6 +78,17 @@ def send_text(message):
 		print("Command is recieved: %s" % message)
 	result = "hello"
 	bot.send_message(message.chat.id, result)
+
+@bot.message_handler(content_types=['contact'])
+def send_contact(message):
+	print(message)
+	print(message.contact)
+
+	result = "%s %s, спасибо, номер телефона получен! +%s" % (message.contact.first_name, message.contact.last_name, message.contact.phone_number)
+	result += "\n"
+	result += "Для оформления электронного пропуска укажите пожалуста причину:"
+
+	bot.send_message(message.chat.id, result, reply_markup=reason_markup)
 
 bot.polling()
 
